78c75123e2ec56cb5d67cc377026bf17b9d1f47b
163002029.junayed <junayednoor@gmail.com>
2022-02-02 09:41:25 +0600

    Divisible By specific Numbers

---
+++ divisible-by.js
@@ -0,0 +1,14 @@
+function divisibleBy(n){
+    for (let i = 1; i <= n; i++) {
+        if(i%3==0 && i%5==0){
+            console.log("Foobar");
+        }else if(i%3==0){
+            console.log("Foo");
+        }else if(i%5==0){
+            console.log("Bar");
+        }else{
+            console.log(i);
+        }
+    }
+}
+divisibleBy(50);
